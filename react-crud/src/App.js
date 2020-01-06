25c980894a3599384808bec67faccb9c97ba4789
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-01-07 00:12:27 +0700

    learning using create react app

---
+++ react-crud/src/App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      newTodo:'',
+      todos: [
+        { id:1, name:"Karate"},
+        {id:2, name:"Judo"},
+        {id:3, name:"Kendo"}      
+      ]
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodos = this.deleteTodos.bind(this);
+
+  }
+
+  handleChange(event){
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
+  addTodo(){
+    const todos = this.state.todos;
+    const newTodo = {
+      name: this.state.newTodo,
+      id: this.state.todos[this.state.todos.length-1].id+1
+    }
+
+    todos.push(newTodo);
+
+    this.setState({
+      todos: todos,
+      newTodo:''
+    })
+  }
+
+  deleteTodos(index){
+    let todos = this.state.todos;
+    delete todos[index];
+    this.setState({todos})
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div className="container">
+          <h2 className="text-center p-4">Todos App</h2>
+          <input 
+            name="todo"
+            type="text"
+            className="my-4 form-control" 
+            placeholder= "Add new to do"
+            onChange={this.handleChange}
+            value={this.state.newTodo}  
+          />
+          <button
+            onClick={this.addTodo}
+            className="mb-4 btn btn-info">
+            Add To Do
+          </button>
+          <ul className="list-group">
+            {this.state.todos.map((item,index)=>{
+              return <li key={item.id}  className="list-group-item">
+                  {item.name} 
+                  <button
+                    onClick={()=>this.deleteTodos(index)} 
+                    className="btn btn-danger btn-sm ml-4">X</button>
+              </li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
